9c5ebc168596244a212c412ddc8202bce20b3230
Rabiul Islam <rabiulislam993@live.com>
2019-07-02 18:59:36 +0600

    added gingonic

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
-import "os"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	sender := NewSender(os.Getenv("_email"), os.Getenv("_password"))
+	email := os.Getenv("email")
+	password := os.Getenv("password")
+	port := os.Getenv("PORT")
+
+	if email == "" {
+		log.Fatal("$EMAIL must be set")
+	}
+	if password == "" {
+		log.Fatal("$PASSWORD must be set")
+	}
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 
 	//The receiver needs to be in slice as the receive supports multiple receiver
-	Receiver := []string{"rabiul@gmail.com", "rabiulislam993@live.com"}
-
+	Receiver := []string{"rabiul@aadibd.com"}
 	Subject := "Testing HTLML Email from golang"
 	message := `
 	<!DOCTYPE HTML PULBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
@@ -24,7 +42,23 @@ func main() {
 	</body>
 	</html>
 	`
-	bodyMessage := sender.WriteHTMLEmail(Receiver, Subject, message)
 
-	sender.SendMail(Receiver, Subject, bodyMessage)
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.GET("/send", func(c *gin.Context) {
+		sender := NewSender(email, password)
+		bodyMessage := sender.WriteHTMLEmail(Receiver, Subject, message)
+		sender.SendMail(Receiver, Subject, bodyMessage)
+
+		c.String(http.StatusOK, string("email sent"))
+	})
+
+	router.Run(":" + port)
 }
